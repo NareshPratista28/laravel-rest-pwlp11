40c0f7f702d124c1bda2d445400062343480cdc2
NareshPratista28 <2141720057@student.polinema.ac.id>
2023-05-31 10:54:57 +0700

    Pertemuan 11 - praktikum 1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ApiAuthController;
 use App\Http\Resources\MahasiswaResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,6 +27,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::apiResource('/mahasiswa', MahasiswaController::class);
+Route::post('/login', [ApiAuthController::class, 'login']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
